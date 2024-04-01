d8a2143b946bd2079b2f7bb53c98f359993ea737
sm3tanus <smetanus3@gmail.com>
2024-04-01 13:41:36 +0300

    Add project files.

---
+++ NyamNyamLina/DBconnection/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyamNyamLina.DBconnection
+{
+    public class Connection
+    {
+        public static NyamNyam_Session_LianaEntities nyamNyam = new NyamNyam_Session_LianaEntities();
+    }
+}
---
+++ NyamNyamLina/Pages/ListofDishes.xaml.cs
@@ -0,0 +1,31 @@
+﻿using NyamNyamLina.DBconnection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NyamNyamLina.Pages
+{
+    /// <summary>
+    /// Interaction logic for ListofDishes.xaml
+    /// </summary>
+    public partial class ListofDishes : Page
+    {
+        public ListofDishes()
+        {
+            InitializeComponent();
+            CategoryCb.ItemsSource = DBconnection.Connection.nyamNyam.Category.ToList();
+            dishesLv.ItemsSource = Connection.nyamNyam.Dish.ToList();
+        }
+    }
+}
